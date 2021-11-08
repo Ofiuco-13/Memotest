e93d53696e19e84f2e1d365d6ad919fd8fff4689
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-11-08 15:13:13 -0300

    Agrega vidas del jugador

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-* {
-    
-}
+const $seccion = document.querySelector('section');
+const $vidasDelJugador = document.querySelector('span');
+const vidasDelJugador = 10;
+
+$vidasDelJugador.textContent = vidasDelJugador;
