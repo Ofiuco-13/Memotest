b406325ebbfbe38dc8200a39435ce4e106df1c6b
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-11-09 14:12:19 -0300

    Aleatorizar objetos
    Cada vez que se recargue la pagina el array devolverá sus objetos ordenados de manera distinta a la anterior.

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-const $seccion = document.querySelector('section');
-const $vidasDelJugador = document.querySelector('span');
+const $seccion = document.querySelector("section");
+const $vidasDelJugador = document.querySelector("span");
 const vidasDelJugador = 10;
 
 $vidasDelJugador.textContent = vidasDelJugador;
 
-//generar data
+//generar datos
 const obtenerDatos = () => [
-    { imgSrc: '.../images.beatles.jpeg', name: 'beatles' },
-    { imgSrc: '.../images.blink182.jpeg', name: 'blink182' },
-    { imgSrc: '.../images/fkatwigs.jpeg', name: 'fka twigs' },
-    { imgSrc: '.../images/fleetwood.jpeg', name: 'fleetwood' },
-    { imgSrc: '.../images.joy-division.jpeg', name: 'joy division' },
-    { imgSrc: '.../images.ledzep.jpeg', name: 'led zeppelin' },
-    { imgSrc: '.../images.metallica.jpeg', name: 'metallica' },
-    { imgSrc: '...images.pinkfloyd.jpeg', name: 'pink floyd' },
-    { imgSrc: '.../images.beatles.jpeg', name: 'beatles' },
-    { imgSrc: '.../images.blink182.jpeg', name: 'blink182' },
-    { imgSrc: '.../images/fkatwigs.jpeg', name: 'fka twigs' },
-    { imgSrc: '.../images/fleetwood.jpeg', name: 'fleetwood' },
-    { imgSrc: '.../images.joy-division.jpeg', name: 'joy division' },
-    { imgSrc: '.../images.ledzep.jpeg', name: 'led zeppelin' },
-    { imgSrc: '.../images.metallica.jpeg', name: 'metallica' },
-    { imgSrc: '...images.pinkfloyd.jpeg', name: 'pink floyd' },
+  { imgSrc: ".../images.beatles.jpeg", name: "beatles" },
+  { imgSrc: ".../images.blink182.jpeg", name: "blink182" },
+  { imgSrc: ".../images/fkatwigs.jpeg", name: "fka twigs" },
+  { imgSrc: ".../images/fleetwood.jpeg", name: "fleetwood" },
+  { imgSrc: ".../images.joy-division.jpeg", name: "joy division" },
+  { imgSrc: ".../images.ledzep.jpeg", name: "led zeppelin" },
+  { imgSrc: ".../images.metallica.jpeg", name: "metallica" },
+  { imgSrc: "...images.pinkfloyd.jpeg", name: "pink floyd" },
+  { imgSrc: ".../images.beatles.jpeg", name: "beatles" },
+  { imgSrc: ".../images.blink182.jpeg", name: "blink182" },
+  { imgSrc: ".../images/fkatwigs.jpeg", name: "fka twigs" },
+  { imgSrc: ".../images/fleetwood.jpeg", name: "fleetwood" },
+  { imgSrc: ".../images.joy-division.jpeg", name: "joy division" },
+  { imgSrc: ".../images.ledzep.jpeg", name: "led zeppelin" },
+  { imgSrc: ".../images.metallica.jpeg", name: "metallica" },
+  { imgSrc: "...images.pinkfloyd.jpeg", name: "pink floyd" },
 ];
 
-const datos = obtenerDatos();
+const aleatorizar = () => {
+  const datos = obtenerDatos();
+  datos.sort( () => Math.random() - 0.5);
+  console.log(datos);
+};
+
+aleatorizar();
